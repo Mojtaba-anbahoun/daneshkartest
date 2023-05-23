ddf828be20ecf2a806ab3aada7e0eaaade70f418
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-05-23 20:03:29 +0330

    "ok"

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+class Discount_Error(Exception):
+    pass
+
+
 def apply_discount(price, discount):
     final_price = int(price*(1 - discount))
     if not 0 <= final_price < price:
-        raise ValueError("Invalid Discount")
+        raise Discount_Error("Invalid Discount")
     return final_price
-
-print()
